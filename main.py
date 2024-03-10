61fe542720056b5c63a71c5e3ef3e8c27dbb28e1
w-winiarz <winiarzwojtek@gmail.com>
2024-03-10 11:37:39 +0100

    LAB3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+from math import *
 import sys
 # a=56
 # print(a)
@@ -131,14 +133,90 @@ import sys
 #         licznik+=1
 # else:
 #     print('licznik')
-licznik=0
-lista=[1,2,3,4]
-a=int(input('Wprowadz liczbe'))
-while licznik<len(lista):
-    if(a-lista[licznik]==0):
-        break
-    licznik+=1
-else:
-    print("liczba nie jest z listy")
+# licznik=0
+# lista=[1,2,3,4]
+# a=int(input('Wprowadz liczbe'))
+# while licznik<len(lista):
+#     if(a-lista[licznik]==0):
+#         break
+#     licznik+=1
+# else:
+#     print("liczba nie jest z listy")
+
+# LAB3
+# try:
+#     a = int(input())
+#     b = int(input())
+#     result = a / b
+#     print(result)
+# except ZeroDivisionError:
+#     print("Error")
+# except ValueError:
+#     print("Wrong value")
+# except Exception:
+#     print("Error")
+# else:
+#     print (result)
+
+# 2 mozliwosci ten sam wynik
+# list1 =[1,2,3,4,5,6,7,8,9]
+# l2=[]
+# for i in list1:
+#     l2.append(pow(i,2))
+# print(l2)
+# l2=[x**2 for x in list1]
+# print (l2)
+# l3=[3**y for y in range(0,6)]
+# print(l3)
+# l4=[x for x in l2 if x % 2== 1]
+# print(l4)
+# l5=[(x,y) for x in l3 for y in l4]
+# print(l5)
+# l6=[]
+# for x in l3:
+#     for y in l4:
+#         l6.append((x,y))
+# print(l6)
+# s1={1: 2,2: 3,3: 4,4: 5}
+# s2={}
+# for key,value in s1.items():
+#     s2[value]=key
+# print(s1)
+# print(s2)
+# s3={v:k for k, v in s1.items()}
+# print(s3)
+
+# def rownanie_kwadratowe(a, b, c):
+#     delta=b**2-4*a*c
+#     if delta<0:
+#         print("Brak pierwiastków")
+#         return 0
+#     elif delta==0:
+#         print("Jeden pierwiastek")
+#
+#         x=(-b)/(2*a)
+#         return x
+#     elif delta>0:
+#         print("Dwa pierwiastki")
+#         # mozna bez math
+#         x1 = (-b + math.sqrt((delta)) / (2 * a))
+#         x2 = (-b - math.sqrt((delta)) / (2 * a))
+#         return x1,x2
+# print(rownanie_kwadratowe(6, 1, 3))
+# print(rownanie_kwadratowe(1, 2, 1))
+# print(rownanie_kwadratowe(1, 4, 2))
+
+# def dlugosc_odcinka(x1=1,x2=2,y1=3,y2=4):
+#     return sqrt((x1-x2)**2+(y1-y2)**2)
+#
+# print(dlugosc_odcinka())
+#
+# print(dlugosc_odcinka(4,2,6,3))
+#
+# print(dlugosc_odcinka(y2=5,y1=5,x2=2, x1=8))
+#
+# print(dlugosc_odcinka(3,5,y2=4, y1=3))
+
+plik=open()
 
 
